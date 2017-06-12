81fb0573bf5882ce2677ef4486aea840129eba16
augusto.moura <augustomoura@zgsolucoes.com.br>
2017-06-12 09:31:49 -0300

    Refatorado categorizador para que gere menos instâncias quando testando se está contido em uma mão

--- src/main/groovy/zg/augusto/gerenciador/CategorizadorJogadas.groovy
+++ src/main/groovy/zg/augusto/gerenciador/CategorizadorJogadas.groovy
@@ -1,38 +1,39 @@
 package zg.augusto.gerenciador
 
-import zg.augusto.combinacoes.Combinacao
-import zg.augusto.combinacoes.CombinacaoDoisPares
-import zg.augusto.combinacoes.CombinacaoFlush
-import zg.augusto.combinacoes.CombinacaoFullHouse
-import zg.augusto.combinacoes.CombinacaoPar
-import zg.augusto.combinacoes.CombinacaoQuadra
-import zg.augusto.combinacoes.CombinacaoRoyalStraightFlush
-import zg.augusto.combinacoes.CombinacaoSequencia
-import zg.augusto.combinacoes.CombinacaoStraightFlush
-import zg.augusto.combinacoes.CombinacaoTrinca
+import zg.augusto.combinacoes.*
 import zg.augusto.dominio.PokerHand
 
 class CategorizadorJogadas {
 
-	final Combinacao[] combinacoes
+	static private final Class<Combinacao>[] COMBINACOES_POSSIVEIS = [
+		CombinacaoRoyalStraightFlush,
+		CombinacaoStraightFlush,
+		CombinacaoQuadra,
+		CombinacaoFullHouse,
+		CombinacaoFlush,
+		CombinacaoSequencia,
+		CombinacaoTrinca,
+		CombinacaoDoisPares,
+		CombinacaoPar,
+	]
+
+	final Combinacao melhorCombinacao
 	final PokerHand mao
 
 	CategorizadorJogadas(PokerHand mao) {
 		this.mao = mao
-		this.combinacoes = [
-			new CombinacaoRoyalStraightFlush(mao),
-			new CombinacaoStraightFlush(mao),
-			new CombinacaoQuadra(mao),
-			new CombinacaoFullHouse(mao),
-			new CombinacaoFlush(mao),
-			new CombinacaoSequencia(mao),
-			new CombinacaoTrinca(mao),
-			new CombinacaoDoisPares(mao),
-			new CombinacaoPar(mao),
-		]
+		this.melhorCombinacao = calcularMelhorJogada()
 	}
 
 	Combinacao calcularMelhorJogada() {
-		return combinacoes.find { it.temCombinacao() }
+		for (Class<Combinacao> comb in COMBINACOES_POSSIVEIS) {
+			final Combinacao auxCombinacao = comb.newInstance(mao)
+
+			if (auxCombinacao.temCombinacao()) {
+				return auxCombinacao
+			}
+		}
+
+		return new CombinacaoMaiorCarta(mao)
 	}
 }
