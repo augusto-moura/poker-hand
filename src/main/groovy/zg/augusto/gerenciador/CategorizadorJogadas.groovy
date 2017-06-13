989993aa08b6a9df6ce80feab7bd49b631a7ee3f
augusto.moura <augustomoura@zgsolucoes.com.br>
2017-06-13 10:50:43 -0300

    Refatoração Categorizador
    Aproveitando as features do Groovy para deixar o código mais limpo

--- src/main/groovy/zg/augusto/gerenciador/CategorizadorJogadas.groovy
+++ src/main/groovy/zg/augusto/gerenciador/CategorizadorJogadas.groovy
@@ -5,25 +5,20 @@ import zg.augusto.dominio.PokerHand
 
 class CategorizadorJogadas {
 
-	static private final List<CombinacaoEntry> COMBINACOES_POSSIVEIS = [
-		new CombinacaoEntry(predicado: CombinacaoRoyalStraightFlush.&temCombinacao, classe: CombinacaoRoyalStraightFlush),
-		new CombinacaoEntry(predicado: CombinacaoStraightFlush.&temCombinacao, classe: CombinacaoStraightFlush),
-		new CombinacaoEntry(predicado: CombinacaoQuadra.&temCombinacao, classe: CombinacaoQuadra),
-		new CombinacaoEntry(predicado: CombinacaoFullHouse.&temCombinacao, classe: CombinacaoFullHouse),
-		new CombinacaoEntry(predicado: CombinacaoFlush.&temCombinacao, classe: CombinacaoFlush),
-		new CombinacaoEntry(predicado: CombinacaoSequencia.&temCombinacao, classe: CombinacaoSequencia),
-		new CombinacaoEntry(predicado: CombinacaoTrinca.&temCombinacao, classe: CombinacaoTrinca),
-		new CombinacaoEntry(predicado: CombinacaoDoisPares.&temCombinacao, classe: CombinacaoDoisPares),
-		new CombinacaoEntry(predicado: CombinacaoPar.&temCombinacao, classe: CombinacaoPar),
+	def static final COMBINACOES_POSSIVEIS = [
+		CombinacaoRoyalStraightFlush,
+		CombinacaoStraightFlush,
+		CombinacaoQuadra,
+		CombinacaoFullHouse,
+		CombinacaoFlush,
+		CombinacaoSequencia,
+		CombinacaoTrinca,
+		CombinacaoDoisPares,
+		CombinacaoPar,
 	]
 
 	static Combinacao calcularMelhorJogada(PokerHand mao) {
-		return (COMBINACOES_POSSIVEIS.find { it.predicado(mao) }?.classe?.newInstance(mao)) ?: (new CombinacaoMaiorCarta(mao))
-	}
-
-	private static class CombinacaoEntry {
-		Closure<Boolean> predicado
-		Class<Combinacao> classe
+		return (COMBINACOES_POSSIVEIS.find { it.temCombinacao(mao) })?.newInstance(mao) ?: (new CombinacaoMaiorCarta(mao))
 	}
 
 }
